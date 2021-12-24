59daef96544e8273f42ae114668efa057bffd097
quantuumstudio <selfdeterminedhermit@gmail.com>
2021-12-24 13:24:44 +0000

    ohkaiVoh5Ee

--- _/1.js
+++ _/1.js
@@ -1,18 +1,23 @@
 const skriptArgs = process.argv.slice(2);
 
+let skriptprocess=skriptArgs.toString().split("").toString();
+
 function sum_of_all_multiples_of_x(x, t_max) {
-    var i = 0; var ret = 0;
-    for (i = 1; i < t_max; i += 1) {if (i % x === 0) {ret += i;}}
+    var i = 0; var ret = 0; 
+    for (i = 1; i < t_max; i += 1) 
+    {if (i % x === 0) {ret += i;}} 
     return ret;
 }
 
-console.log(sum_of_all_multiples_of_x(parseInt(skriptArgs[0]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[1]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[2]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[3]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[4]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[5]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[6]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[7]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[8]), 1000),
-            sum_of_all_multiples_of_x(parseInt(skriptArgs[9]), 1000));
+console.log(
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[0].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[1].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[2].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[3].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[4].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[5].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[6].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[7].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[8].charCodeAt(0)), 1000),
+    sum_of_all_multiples_of_x(parseInt(skriptprocess[9].charCodeAt(0)), 1000),
+);
